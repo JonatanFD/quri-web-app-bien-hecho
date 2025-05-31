7c11d126749951c261f1505483081d87d4005889
JonatanFD <u20211b995@upc.edu.pe>
2025-05-31 04:45:43 -0500

    feat: Update sidenav layout for better responsiveness and add mobile toggle button

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit, signal } from '@angular/core';
 import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { RouterLinkActive } from '@angular/router';
@@ -6,77 +6,113 @@ import { RouterLink } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { filter } from 'rxjs';
+import { MediaMatcher } from '@angular/cdk/layout';
 
 export interface AppRoute {
-  path: string;
-  label: string;
-  icon: string;
+    path: string;
+    label: string;
+    icon: string;
 }
 
 @Component({
-  selector: 'app-root',
-  imports: [
-    RouterOutlet,
-    MatSidenavModule,
-    RouterLinkActive,
-    RouterLink,
-    MatButtonModule,
-    MatIconModule,
-  ],
-  templateUrl: './app.html',
-  styleUrl: './app.css',
+    selector: 'app-root',
+    imports: [
+        RouterOutlet,
+        MatSidenavModule,
+        RouterLinkActive,
+        RouterLink,
+        MatButtonModule,
+        MatIconModule,
+    ],
+    templateUrl: './app.html',
+    styleUrl: './app.css',
 })
-export class App {
-  protected title = 'quri-web-app-bien-hecho';
-  currentPage = '';
-  currentPath = '';
-  routes: AppRoute[] = [
-    {
-      path: 'home',
-      label: 'Home',
-      icon: 'home',
-    },
-    {
-      path: 'dashboard',
-      label: 'Dashboard',
-      icon: 'dashboard',
-    },
-    {
-      path: 'templates',
-      label: 'Templates',
-      icon: 'app_registration',
-    },
-    {
-      path: 'favorites',
-      label: 'Favorites',
-      icon: 'favorite',
-    },
-    {
-      path: 'explore',
-      label: 'Explore',
-      icon: 'explore',
-    },
-    {
-      path: 'design-lab',
-      label: 'Design Lab',
-      icon: 'brush',
-    },
-    {
-      path: 'settings',
-      label: 'Settings',
-      icon: 'settings',
-    },
-  ];
-  constructor(private router: Router) {
-    this.router.events
-      .pipe(filter((event) => event instanceof NavigationEnd))
-      .subscribe(() => {
-        const currentPath = this.router.url.substring(1);
-        this.currentPath = this.router.url;
-        const currentPage = this.routes.find((p) =>
-          currentPath.startsWith(p.path)
+export class App implements OnDestroy, OnInit {
+    protected title = 'quri-web-app-bien-hecho';
+    protected readonly isMobile = signal(true);
+    protected readonly isOpened = signal(true);
+    private readonly _mobileQuery: MediaQueryList;
+    private readonly _mobileQueryListener: () => void;
+
+    currentPage = '';
+    currentPath = '';
+    routes: AppRoute[] = [
+        {
+            path: 'home',
+            label: 'Home',
+            icon: 'home',
+        },
+        {
+            path: 'dashboard',
+            label: 'Dashboard',
+            icon: 'dashboard',
+        },
+        {
+            path: 'templates',
+            label: 'Templates',
+            icon: 'app_registration',
+        },
+        {
+            path: 'favorites',
+            label: 'Favorites',
+            icon: 'favorite',
+        },
+        {
+            path: 'explore',
+            label: 'Explore',
+            icon: 'explore',
+        },
+        {
+            path: 'design-lab',
+            label: 'Design Lab',
+            icon: 'brush',
+        },
+        {
+            path: 'settings',
+            label: 'Settings',
+            icon: 'settings',
+        },
+    ];
+    constructor(private router: Router) {
+        this.router.events
+            .pipe(filter((event) => event instanceof NavigationEnd))
+            .subscribe(() => {
+                const currentPath = this.router.url.substring(1);
+                this.currentPath = this.router.url;
+                const currentPage = this.routes.find((p) =>
+                    currentPath.startsWith(p.path)
+                );
+                this.currentPage = currentPage ? currentPage.label : '';
+            });
+
+        const media = inject(MediaMatcher);
+
+        this._mobileQuery = media.matchMedia('(max-width: 600px)');
+        this.isMobile.set(this._mobileQuery.matches);
+        this._mobileQueryListener = () => {
+            this.updateLayout();
+        };
+        this._mobileQuery.addEventListener('change', this._mobileQueryListener);
+    }
+
+    updateLayout() {
+        if (!this._mobileQuery.matches) {
+            this.isOpened.set(true);
+        }
+        if (this._mobileQuery.matches && this.isOpened() === true) {
+            this.isOpened.set(false);
+        }
+
+        this.isMobile.set(this._mobileQuery.matches);
+    }
+
+    ngOnDestroy(): void {
+        this._mobileQuery.removeEventListener(
+            'change',
+            this._mobileQueryListener
         );
-        this.currentPage = currentPage ? currentPage.label : '';
-      });
-  }
+    }
+    ngOnInit(): void {
+        this.updateLayout();
+    }
 }
