f2550bfb970bf5ec8be8b5e04e648830a9a3c704
JonatanFD <u20211b995@upc.edu.pe>
2025-05-31 04:12:13 -0500

    feat: add environment configuration files for development and production

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production : false,
+  apiBaseUrl: 'http://localhost:3000',
+  projectsApiBaseUrl: 'http://localhost:3000/projects',
+  cloudinaryApiBaseUrl: 'https://api.cloudinary.com/v1_1/dkkfv72vo/image',
+  defaultUserId: 'user-001',
+  defaultPreviewImageUrl: 'https://picsum.photos/150/150',
+  defaultProjectStatus: 'blueprint',
+  defaultGarmentSize: 'M',
+  defaultGarmentColor: '#FFFFFF',
+  garmentColorImagesUrl: 'https://res.cloudinary.com/dkkfv72vo/image/upload/v1747000549/Frame_530_hfhrko.webp',
+};
