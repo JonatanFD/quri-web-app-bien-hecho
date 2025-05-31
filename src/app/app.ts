266eb02e275baa221da56ad15c4c9a73a060d1ab
JonatanFD <u20211b995@upc.edu.pe>
2025-05-31 04:12:24 -0500

    feat: Implement responsive layout with sidenav and toolbar
    - Added app.css for styling the application layout, including sidenav and toolbar.
    - Refactored app.html to include a sidenav with navigation items and a toolbar.
    - Updated app.ts to manage routing and current page state.
    - Enhanced index.html to include Material Icons and Roboto font.
    - Modified global styles in styles.css for consistent typography and layout.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,87 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { RouterLinkActive } from '@angular/router';
+import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { filter } from 'rxjs';
+
+export interface AppRoute {
+  path: string;
+  label: string;
+  icon: string;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatSidenavModule,
+    RouterLinkActive,
+    RouterLink,
+    MatButtonModule,
+    MatIconModule,
+  ],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected title = 'quri-web-app-bien-hecho';
+  currentPage = '';
+  currentPath = '';
+  routes: AppRoute[] = [
+    {
+      path: 'home',
+      label: 'Home',
+      icon: 'home',
+    },
+    {
+      path: 'dashboard',
+      label: 'Dashboard',
+      icon: 'dashboard',
+    },
+    {
+      path: 'templates',
+      label: 'Templates',
+      icon: 'app_registration',
+    },
+    {
+      path: 'favorites',
+      label: 'Favorites',
+      icon: 'favorite',
+    },
+    {
+      path: 'explore',
+      label: 'Explore',
+      icon: 'explore',
+    },
+    {
+      path: 'projects',
+      label: 'Projects',
+      icon: 'folder',
+    },
+    {
+      path: 'design-lab',
+      label: 'Design Lab',
+      icon: 'brush',
+    },
+    {
+      path: 'settings',
+      label: 'Settings',
+      icon: 'settings',
+    },
+  ];
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe(() => {
+        const currentPath = this.router.url.substring(1);
+        this.currentPath = this.router.url;
+        const currentPage = this.routes.find((p) =>
+          currentPath.startsWith(p.path)
+        );
+        this.currentPage = currentPage ? currentPage.label : '';
+      });
+  }
 }
